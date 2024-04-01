09fde2aea7985e7a498ad91de883b65d43e871ec
Hientrietman <vanqui8028@gmail.com>
2024-04-01 10:19:47 +0700

    01042024-PQLSV-38-Quý-thêm form tạo tài khoản

--- GUI/TaoTaiKhoan.cs
+++ GUI/TaoTaiKhoan.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class TaoTaiKhoan : Form
     {
-        public Form1()
+        public TaoTaiKhoan()
         {
             InitializeComponent();
         }
