f7422e14f2b758dc615f5a249c037d57eacea59b
Hientrietman <vanqui8028@gmail.com>
2024-04-01 18:32:23 +0700

    01042024-PQLSV-38-Quý-Thêm giao diện quên mật khẩu

--- GUI/TaoTaiKhoan.Designer.cs
+++ GUI/TaoTaiKhoan.Designer.cs
@@ -28,85 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnLogin = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.Klabel_nhapmatkhau = new System.Windows.Forms.Label();
+            this.Klabel_nhaptentaikhoan = new System.Windows.Forms.Label();
+            this.btnXacNhan = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.lbl_login = new System.Windows.Forms.Label();
             this.txtPassWord = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
             this.txtUserName = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
             this.Kcb_chonloaitaikhoan = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Klabel_chonloaitaikhoan = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Kcb_chonloaitaikhoan)).BeginInit();
             this.SuspendLayout();
             // 
-            // label2
+            // Klabel_nhapmatkhau
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.label2.Location = new System.Drawing.Point(45, 210);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 20);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Nhập mật khẩu";
+            this.Klabel_nhapmatkhau.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Klabel_nhapmatkhau.AutoSize = true;
+            this.Klabel_nhapmatkhau.BackColor = System.Drawing.Color.Transparent;
+            this.Klabel_nhapmatkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Klabel_nhapmatkhau.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.Klabel_nhapmatkhau.Location = new System.Drawing.Point(45, 210);
+            this.Klabel_nhapmatkhau.Name = "Klabel_nhapmatkhau";
+            this.Klabel_nhapmatkhau.Size = new System.Drawing.Size(117, 20);
+            this.Klabel_nhapmatkhau.TabIndex = 12;
+            this.Klabel_nhapmatkhau.Text = "Nhập mật khẩu";
             // 
-            // label1
+            // Klabel_nhaptentaikhoan
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.label1.Location = new System.Drawing.Point(45, 141);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 20);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Nhập tên tài khoản";
+            this.Klabel_nhaptentaikhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Klabel_nhaptentaikhoan.AutoSize = true;
+            this.Klabel_nhaptentaikhoan.BackColor = System.Drawing.Color.Transparent;
+            this.Klabel_nhaptentaikhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Klabel_nhaptentaikhoan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.Klabel_nhaptentaikhoan.Location = new System.Drawing.Point(45, 141);
+            this.Klabel_nhaptentaikhoan.Name = "Klabel_nhaptentaikhoan";
+            this.Klabel_nhaptentaikhoan.Size = new System.Drawing.Size(143, 20);
+            this.Klabel_nhaptentaikhoan.TabIndex = 11;
+            this.Klabel_nhaptentaikhoan.Text = "Nhập tên tài khoản";
             // 
-            // btnLogin
+            // btnXacNhan
             // 
-            this.btnLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnLogin.Location = new System.Drawing.Point(304, 304);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.OverrideDefault.Back.Color1 = System.Drawing.Color.White;
-            this.btnLogin.OverrideDefault.Back.Color2 = System.Drawing.Color.White;
-            this.btnLogin.OverrideDefault.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.OverrideDefault.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.OverrideDefault.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            this.btnXacNhan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnXacNhan.Location = new System.Drawing.Point(304, 304);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.OverrideDefault.Back.Color1 = System.Drawing.Color.White;
+            this.btnXacNhan.OverrideDefault.Back.Color2 = System.Drawing.Color.White;
+            this.btnXacNhan.OverrideDefault.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.OverrideDefault.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.OverrideDefault.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.btnLogin.OverrideDefault.Content.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.OverrideDefault.Content.ShortText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.OverrideDefault.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Size = new System.Drawing.Size(95, 28);
-            this.btnLogin.StateCommon.Back.Color1 = System.Drawing.Color.White;
-            this.btnLogin.StateCommon.Back.Color2 = System.Drawing.Color.White;
-            this.btnLogin.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            this.btnXacNhan.OverrideDefault.Content.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.OverrideDefault.Content.ShortText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.OverrideDefault.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXacNhan.Size = new System.Drawing.Size(95, 28);
+            this.btnXacNhan.StateCommon.Back.Color1 = System.Drawing.Color.White;
+            this.btnXacNhan.StateCommon.Back.Color2 = System.Drawing.Color.White;
+            this.btnXacNhan.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.btnLogin.StateCommon.Border.Rounding = 8;
-            this.btnLogin.StateCommon.Border.Width = 2;
-            this.btnLogin.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.btnLogin.StateTracking.Border.Color1 = System.Drawing.Color.White;
-            this.btnLogin.StateTracking.Border.Color2 = System.Drawing.Color.White;
-            this.btnLogin.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            this.btnXacNhan.StateCommon.Border.Rounding = 8;
+            this.btnXacNhan.StateCommon.Border.Width = 2;
+            this.btnXacNhan.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXacNhan.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.btnXacNhan.StateTracking.Border.Color1 = System.Drawing.Color.White;
+            this.btnXacNhan.StateTracking.Border.Color2 = System.Drawing.Color.White;
+            this.btnXacNhan.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
             | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
-            this.btnLogin.StateTracking.Border.Rounding = 8;
-            this.btnLogin.StateTracking.Border.Width = 2;
-            this.btnLogin.StateTracking.Content.ShortText.Color1 = System.Drawing.Color.White;
-            this.btnLogin.StateTracking.Content.ShortText.Color2 = System.Drawing.Color.White;
-            this.btnLogin.StateTracking.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.TabIndex = 10;
-            this.btnLogin.Values.Text = "Tạo Tài Khoản";
+            this.btnXacNhan.StateTracking.Border.Rounding = 8;
+            this.btnXacNhan.StateTracking.Border.Width = 2;
+            this.btnXacNhan.StateTracking.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnXacNhan.StateTracking.Content.ShortText.Color2 = System.Drawing.Color.White;
+            this.btnXacNhan.StateTracking.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXacNhan.TabIndex = 10;
+            this.btnXacNhan.Values.Text = "Tạo Tài Khoản";
+            this.btnXacNhan.Click += new System.EventHandler(this.btnXacNhan_Click);
             // 
             // lbl_login
             // 
@@ -166,7 +167,7 @@
             "Giảng Viên",
             "Quản Lý"});
             this.Kcb_chonloaitaikhoan.Location = new System.Drawing.Point(45, 308);
-            this.Kcb_chonloaitaikhoan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Kcb_chonloaitaikhoan.Margin = new System.Windows.Forms.Padding(4);
             this.Kcb_chonloaitaikhoan.Name = "Kcb_chonloaitaikhoan";
             this.Kcb_chonloaitaikhoan.Size = new System.Drawing.Size(225, 21);
             this.Kcb_chonloaitaikhoan.StateCommon.ComboBox.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
@@ -178,34 +179,35 @@
             this.Kcb_chonloaitaikhoan.StateCommon.DropBack.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
             this.Kcb_chonloaitaikhoan.StateCommon.DropBack.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
             this.Kcb_chonloaitaikhoan.TabIndex = 57;
+    
             // 
-            // label3
+            // Klabel_chonloaitaikhoan
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
-            this.label3.Location = new System.Drawing.Point(45, 278);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 20);
-            this.label3.TabIndex = 58;
-            this.label3.Text = "Chọn loại tài khoản";
+            this.Klabel_chonloaitaikhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Klabel_chonloaitaikhoan.AutoSize = true;
+            this.Klabel_chonloaitaikhoan.BackColor = System.Drawing.Color.Transparent;
+            this.Klabel_chonloaitaikhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Klabel_chonloaitaikhoan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(171)))), ((int)(((byte)(150)))));
+            this.Klabel_chonloaitaikhoan.Location = new System.Drawing.Point(45, 278);
+            this.Klabel_chonloaitaikhoan.Name = "Klabel_chonloaitaikhoan";
+            this.Klabel_chonloaitaikhoan.Size = new System.Drawing.Size(144, 20);
+            this.Klabel_chonloaitaikhoan.TabIndex = 58;
+            this.Klabel_chonloaitaikhoan.Text = "Chọn loại tài khoản";
             // 
             // TaoTaiKhoan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(436, 571);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Klabel_chonloaitaikhoan);
             this.Controls.Add(this.Kcb_chonloaitaikhoan);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.Klabel_nhapmatkhau);
+            this.Controls.Add(this.Klabel_nhaptentaikhoan);
+            this.Controls.Add(this.btnXacNhan);
             this.Controls.Add(this.lbl_login);
             this.Controls.Add(this.txtPassWord);
             this.Controls.Add(this.txtUserName);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "TaoTaiKhoan";
             this.Text = "Tạo tài khoản";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -217,13 +219,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private ComponentFactory.Krypton.Toolkit.KryptonButton btnLogin;
+        private System.Windows.Forms.Label Klabel_nhapmatkhau;
+        private System.Windows.Forms.Label Klabel_nhaptentaikhoan;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnXacNhan;
         private System.Windows.Forms.Label lbl_login;
         private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtPassWord;
         private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtUserName;
         private ComponentFactory.Krypton.Toolkit.KryptonComboBox Kcb_chonloaitaikhoan;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Klabel_chonloaitaikhoan;
     }
 }
