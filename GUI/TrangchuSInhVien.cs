307c7389becde5928cb93082d03ae79e1bdd4c5c
Yen Linh <Yen Linh@ADMIN>
2024-04-02 20:54:45 +0700

    giao diện trang chủ

---
+++ GUI/TrangchuSInhVien.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class TrangchuSInhVien : Form
+    {
+        public TrangchuSInhVien()
+        {
+            InitializeComponent();
+        }
+
+        private void lblMSSV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlMenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrangchuSInhVien_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangXuat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
