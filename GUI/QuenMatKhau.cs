c9b825344e5c628f93c20d7a0f122888b05a5d9b
Hientrietman <vanqui8028@gmail.com>
2024-04-04 10:11:36 +0700

    04042024-PQLSV-8-Quý-Xử lý gửi email sau khi đã kiểm tra mail có tồn tại trong csdl không

--- GUI/QuenMatKhau.cs
+++ GUI/QuenMatKhau.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BUS;
+using System;
+
 using System.Windows.Forms;
 
 namespace GUI
@@ -16,7 +11,28 @@ namespace GUI
         {
             InitializeComponent();
         }
+        
+        private void btnGuiMa_Click(object sender, EventArgs e)
+        {
+            if (QuenMatKhauBUS.Instance.KiemTraEmail(txtEmail.Text))
+            {
+                string email = txtEmail.Text; // Lấy địa chỉ email từ textbox
+                string otp = QuenMatKhauBUS.Instance.GenerateOTP(); // Tạo mã OTP
+                QuenMatKhauBUS.Instance.SendEmail(email, otp); // Gửi email chứa mã OTP
 
-     
+                // Hiển thị thông báo cho người dùng
+                MessageBox.Show("Mã OTP đã được gửi đến địa chỉ email của bạn.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy địa chỉ mail trên, vui lòng kiểm tra hoặc cập nhật lại địa chỉ mail", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void btnXacNhan_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
